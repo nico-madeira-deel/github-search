b68979605d28174baa6689aa3cf75103dd79e86c
Nicolle Madeira <nicollemguimaraes@gmail.com>
2020-09-03 01:05:46 -0300

    change storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,10 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   "stories": ["../src/components/**/stories.tsx"],
-  "addons": ["@storybook/addon-essentials"]
+  "addons": ["@storybook/addon-essentials"],
+  webpackFinal: async (config) => {
+    config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+    return config;
+  },
 }
