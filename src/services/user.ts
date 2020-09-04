09a7c7e74d0cfacee5c8a1092595efb0f9ac30e1
Nicolle Madeira <nicollemguimaraes@gmail.com>
2020-09-04 02:11:43 -0300

    import types from models folder

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,29 +1,5 @@
 import api from './api'
-
-export type UserResponse = {
-  avatar_url?: string
-  bio?: string
-  email?: string
-  followers?: number
-  following?: number
-  id: number
-  name?: string
-}
-
-type OwnerUserRepositoriesResponse = {
-  id: number
-}
-
-export type UserRepositoriesResponse = {
-  description?: string
-  forks: number
-  html_url: string
-  id: number
-  name: string
-  owner: OwnerUserRepositoriesResponse
-  stargazers_count: number
-  watchers: number
-}
+import { UserResponse, UserRepositoriesResponse } from 'models/user'
 
 export function getUserData(username: string) {
   return api.get<UserResponse>(`/users/${username}`)
